4e76ceda3521ace5136643a1c704735e7a277729
liang yutang <957753243@qq.com>
2023-07-06 15:40:46 +0800

    创建ObjectActor

--- Source/FPPShoot/Private/FPSObjectActor.cpp
+++ Source/FPPShoot/Private/FPSObjectActor.cpp
@@ -3,12 +3,20 @@
 
 #include "FPSObjectActor.h"
 
+#include "Components/SphereComponent.h"
+
 // Sets default values
 AFPSObjectActor::AFPSObjectActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+
+	MeshComponent=CreateDefaultSubobject<UStaticMeshComponent>("MeshComponent");
+	RootComponent=MeshComponent;
+
+	SphereComponent=CreateDefaultSubobject<USphereComponent>("SphereComponent");
+	RootComponent->SetupAttachment(MeshComponent);
 }
 
 // Called when the game starts or when spawned
--- Source/FPPShoot/Public/FPSObjectActor.h
+++ Source/FPPShoot/Public/FPSObjectActor.h
@@ -14,7 +14,14 @@ class FPPSHOOT_API AFPSObjectActor : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AFPSObjectActor();
+	
+protected:
+	UPROPERTY(VisibleAnywhere,Category="Components")
+	UStaticMeshComponent* MeshComponent;
 
+	UPROPERTY(VisibleAnywhere,Category="Components")
+	class USphereComponent* SphereComponent;
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
